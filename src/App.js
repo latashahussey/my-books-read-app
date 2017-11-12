508b2265efea2bb9d81e36b2de7eab9547fd8948
LaTasha Hussey <latasha.hussey@gmail.com>
2017-11-12 19:29:33 +0000

    refactor Currently Reading bookshelf to pull data from BooksAPI

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import ListBookshelves from './ListBookshelves'
 import SearchBooks from './SearchBooks'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {
-    books: []
-  }
+	state = {
+   		books: []
+  	}
+	
+	// Grab all book objects from BooksAPI and store in array
+	componentDidMount() {
+      BooksAPI.getAll().then((books) => {
+      	this.setState({books})
+		})
+    }
 
 // Use React Router to route user to correct UI
   render() {
     return (
       <div className="app">
        	<Route exact path='/' render={()=> (
-    		<ListBookshelves />
+    		<ListBookshelves books={this.state.books}/>
     	)}/>
 		
 		<Route path='/search' render={()=> (
