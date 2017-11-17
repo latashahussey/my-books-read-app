1589284952a7925fee4fe9746a8af7925d3815dc
sugarcookie <latasha.hussey@gmail.com>
2017-11-17 15:00:25 -0600

    move handleShelfChange method to Bookshelf component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,12 +1,35 @@
+// Component that represents a digital bookshelf
+
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 class Bookshelf extends Component {
 
-  render () {
-    return (
-      
-    )
+    // Required prop types
+    static propTypes = {
+      books: PropTypes.array.isRequired,
+      onUpdateBookshelf: PropTypes.func.isRequired
+    }
+
+    /*
+        @description Handles book's bookshelf change
+        @param bookId {string} Unique string for books
+        @param e {string} The bookshelf
+     */
+    handleShelfChange = (bookId, e) => {
+      const shelf = e.target.value
+      if (this.props.onUpdateBookshelf)
+        this.props.onUpdateBookshelf(bookId, shelf)
+    }
+
+    // Display the bookshelf
+    render () {
+        return (
+
+
+
+
+        )
   }
 }
