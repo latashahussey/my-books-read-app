9b4315279f431a3f9a2c1ffc2d58997fac1ad71e
sugarcookie <latasha.hussey@gmail.com>
2017-11-18 16:58:18 -0600

    clean code and added comments

--- src/App.js
+++ src/App.js
@@ -6,52 +6,68 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {
-    books: [],
-    searchResults: []
-  }
-
-  // Grab all book objects from BooksAPI and store in array
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({books})
-    })
-  }
-
-  updateBookshelf = (book, shelf) => {
-    console.log(book)
-    console.log(shelf)
-    BooksAPI.update(book, shelf)
-    BooksAPI.getAll().then((books) => {
-      this.setState({books})
-    })
-  }
-
-  searchBooks = (query, maxResults) => {
-    if (query.length !== 0) {
-      BooksAPI.search(query, maxResults).then((searchResults) => {
-        this.setState({searchResults})
-      })
-    } else {
-      BooksAPI.getAll().then((books) => {
-        this.setState({books})
-			})
+    // Initialize state
+    state = {
+        books: [],
+        searchResults: []
     }
-	}
-    // Use React Router to route user to correct UI
+
+    // Grab all book objects from remote BooksAPI and store them in an array
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({books})
+        })
+    }
+
+    /*
+        @description updateBookshelf Handles updates to bookshelf
+        @param book {object} Book stored on bookshelf
+        @param shelf {string} The bookshelf
+     */
+    updateBookshelf = (book, shelf) => {
+        console.log(book)
+        console.log(shelf)
+        BooksAPI.update(book, shelf)
+        BooksAPI.getAll().then((books) => {
+            this.setState({books})
+        })
+    }
+
+    /*
+        @description searchBooks Handles book search feature
+        @param query {string} Search query
+        @param maxResults {integer}  Max number of search results
+     */
+    searchBooks = (query, maxResults) => {
+        if (query.length !== 0) {
+            BooksAPI.search(query, maxResults).then((searchResults) => {
+                this.setState({searchResults})
+            })
+        } else {
+            BooksAPI.getAll().then((books) => {
+                this.setState({books})
+            })
+        }
+    }
+
+    // Route user to correct UI
     render() {
-      return (<div className="app">
-        <Route exact path='/' render={() => (<ListBookshelves
-					onUpdateBookshelf={this.updateBookshelf}
-					books={this.state.books}/>)}/>
+        return (<div className="app">
+            <Route exact="exact" path='/' render={() => (
+                <ListBookshelves
+                    onUpdateBookshelf={this.updateBookshelf}
+                    books={this.state.books}/>)
+                }/>
 
-        <Route path='/search' render={() => (<SearchBooks
-					onSearchBooks={this.searchBooks}
-					searchResults={this.state.searchResults}
-					onUpdateBookshelf={this.updateBookshelf}
-					books={this.state.books}/>)}/>
-      </div>)
+            <Route path='/search' render={() => (
+                <SearchBooks
+                    onSearchBooks={this.searchBooks}
+                    searchResults={this.state.searchResults}
+                    onUpdateBookshelf={this.updateBookshelf}
+                    books={this.state.books}/>)
+                }/>
+        </div>)
     }
-  }
+}
 
-  export default BooksApp
+export default BooksApp
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -3,73 +3,85 @@ import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 class SearchBooks extends Component {
+    // Required property types for component
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        searchResults: PropTypes.array.isRequired,
+        onSearchBooks: PropTypes.func.isRequired,
+        onUpdateBookshelf: PropTypes.func.isRequired
+    }
 
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    searchResults: PropTypes.array.isRequired,
-    onSearchBooks: PropTypes.func.isRequired,
-    onUpdateBookshelf: PropTypes.func.isRequired
-  }
+    /*
+		@description handleSearchTerms Handles book search
+		@param e {string} Search term
+	 */
+    handleSearchTerms = (e) => {
+        if (this.props.onSearchBooks)
+            this.props.onSearchBooks(e.target.value)
+    }
 
-  handleSearchTerms = (e) => {
-    if (this.props.onSearchBooks)
-      this.props.onSearchBooks(e.target.value)
-  }
+    /*
+		@description handleShelfChange Handles book's bookshelf change
+		@param bookId {string} Unique string for books
+		@param e {string} The bookshelf
+	 */
+    handleShelfChange = (bookId, e) => {
+        const shelf = e.target.value
+        if (this.props.onUpdateBookshelf)
+            this.props.onUpdateBookshelf(bookId, shelf)
+    }
 
-  handleShelfChange = (bookId, e) => {
-    const shelf = e.target.value
-    if (this.props.onUpdateBookshelf)
-      this.props.onUpdateBookshelf(bookId, shelf)
-  }
+    // Display search results
+    render() {
+        // Parameter for this component
+        const {searchResults} = this.props
 
-  render() {
-    const {searchResults} = this.props
-
-    return (<div className="search-books">
-      <div className="search-books-bar">
-        <Link to="/" className="close-search">Close</Link>
-        <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" onChange={this.handleSearchTerms.bind(this)}/>
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/" className="close-search">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author" onChange={this.handleSearchTerms.bind(this)}/>
+                    </div> {/* ./search-books-input-wrapper */}
+                </div> {/* ./search-books */}
+            <div className="search-books-results">
+                <ol className="books-grid">
+                    {searchResults.map((book) => (
+                        <li key={book.id}>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{
+                                            width: 128,
+                                            height: 193,
+                                            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                                        }}>
+                                </div>
+                                    {/* ./book-cover */}
+                                    <div className="book-shelf-changer">
+                                        <select value={book.shelf} onChange={this.handleShelfChange.bind(this, book)}>
+                                            <option value="none" disabled="disabled">Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none" selected>None</option>
+                                        </select>
+                                    </div>
+                                    {/* ./book-shelf-changer */}
+                                </div>
+                                {/* ./book-top */}
+                                <div className="book-title">{book.title}</div>
+                                {book.authors.map((author) => (<div key={author} className="book-authors">{author}</div>))}
+                            </div>
+                            {/* ./book */}
+                        </li>))
+                    }
+                    {/* ./books.filter.map */}
+                </ol>
+            </div>
         </div>
-      </div>
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {
-            searchResults.map((book) => (<li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                    }}></div>
-                  {/* ./book-cover */}
-                  <div className="book-shelf-changer">
-                    <select value={book.shelf} onChange={this.handleShelfChange.bind(this, book)}>
-                      <option value="none" disabled="disabled">Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                  {/* ./book-shelf-changer */}
-                </div>
-                {/* ./book-top */}
-                <div className="book-title">{book.title}</div>
-                {book.authors.map((author) => (<div key={author} className="book-authors">{author}</div>))}
-              </div>
-              {/* ./book */}
-            </li>))
-          }
-          {/* ./books.filter.map */}
+    )
 
-        </ol>
-
-      </div>
-    </div>)
-
-  }
+    }
 
 }
 
