ad59bc8ccf301f04c2a4095b578d9c7d9a7076b5
sugarcookie <latasha.hussey@gmail.com>
2017-11-19 09:33:00 -0600

    remove selected option from shelf change option

--- src/ListBookshelves.js
+++ src/ListBookshelves.js
@@ -56,7 +56,7 @@ class ListBookshelves extends Component {
 			                                                <option value="currentlyReading">Currently Reading</option>
 			                                                <option value="wantToRead">Want to Read</option>
 			                                                <option value="read">Read</option>
-			                                                <option value="none" selected="selected">None</option>
+			                                                <option value="none">None</option>
 			                                            </select>
 			                                        </div>
 			                                        {/* ./book-shelf-changer */}
@@ -103,7 +103,7 @@ class ListBookshelves extends Component {
 				                                            <option value="currentlyReading">Currently Reading</option>
 				                                            <option value="wantToRead">Want to Read</option>
 				                                            <option value="read">Read</option>
-				                                            <option value="none" selected>None</option>
+				                                            <option value="none">None</option>
 				                                        </select>
 				                                    </div>
 				                                    {/* ./book-shelf-changer */}
