c1ead49c774723d151d1bacc01ba61d3d7a5c536
sugarcookie <tasha@lhussey.com>
2017-11-23 12:32:19 -0600

    refactored SearchBooks component

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Book from './Book'
 
 class SearchBooks extends Component {
     // Required property types for component
@@ -20,17 +21,6 @@ class SearchBooks extends Component {
             this.props.onSearchBooks(e.target.value)
     }
 
-    /*
-		@description handleShelfChange Handles book's bookshelf change
-		@param bookId {string} Unique string for books
-		@param e {string} The bookshelf
-	 */
-    handleShelfChange = (bookId, e) => {
-        const shelf = e.target.value
-        if (this.props.onUpdateBookshelf)
-            this.props.onUpdateBookshelf(bookId, shelf)
-    }
-
     // Display search results
     render() {
         // Parameter for this component
@@ -44,37 +34,14 @@ class SearchBooks extends Component {
                         <input type="text" placeholder="Search by title or author" onChange={this.handleSearchTerms.bind(this)}/>
                     </div> {/* ./search-books-input-wrapper */}
                 </div> {/* ./search-books */}
-            <div className="search-books-results">
-                <ol className="books-grid">
-                    {searchResults.map((book) => (
-                        <li key={book.id}>
-                            <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{
-                                            width: 128,
-                                            height: 193,
-                                            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                                        }}>
-                                </div>
-                                    {/* ./book-cover */}
-                                    <div className="book-shelf-changer">
-                                        <select value={book.shelf} onChange={this.handleShelfChange.bind(this, book)}>
-                                            <option  disabled="disabled">Move to...</option>
-                                            <option value="currentlyReading">Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
-                                    {/* ./book-shelf-changer */}
-                                </div>
-                                {/* ./book-top */}
-                                <div className="book-title">{book.title}</div>
-                                {book.authors.map((author) => (<div key={author} className="book-authors">{author}</div>))}
-                            </div>
-                            {/* ./book */}
-                        </li>))
-                    }
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {searchResults.map((book) => (
+                            <Book
+                                book={book}
+                                onUpdateBookshelf={this.props.onUpdateBookshelf}
+                            />
+                        ))}
                     {/* ./books.filter.map */}
                 </ol>
             </div>
